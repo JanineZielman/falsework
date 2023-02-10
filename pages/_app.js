6477030b7a3f0a6aae58217d3e7ba0a18f36d1bd
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-02-10 13:08:07 +0100

    preview update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '@/styles/globals.scss'
 import '@/styles/breakpoints.scss'
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "../prismicio";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicPreview repositoryName={repositoryName}>
+      <Component {...pageProps} />
+    </PrismicPreview>
+  )
 }
