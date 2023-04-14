15debff1ef81db1978578256782182466c37b1bb
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-04-14 10:01:13 +0200

    image update

--- slices/TitleText/index.js
+++ slices/TitleText/index.js
@@ -8,16 +8,18 @@ import { PrismicRichText } from '@prismicio/react'
  */
 const TitleText = ({ slice }) => {
   return(
-    <section className='title-text flex'>
-      {slice.items.map((item, i) => {
-        return(
-          <div className='item' key={`item${i}`}>
-            <h2>{item.title}</h2>
-            <PrismicRichText field={item.text}/>
-          </div>
-        )
-      })}
-    </section>
+    <>
+      <section className='title-text flex'>
+        {slice.items.map((item, i) => {
+          return(
+            <div className='item' key={`item${i}`}>
+              <h2>{item.title}</h2>
+              <PrismicRichText field={item.text}/>
+            </div>
+          )
+        })}
+      </section>
+    </>
   );
 }
 
