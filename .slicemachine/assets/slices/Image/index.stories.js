923b5b6936d5ddd1a892558128d600765e8d6a2f
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-04-14 16:32:55 +0200

    update

--- .slicemachine/assets/slices/Image/index.stories.js
+++ .slicemachine/assets/slices/Image/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":640,"height":480},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1606248897732-2c5ffe759c04?rect=0%2C500%2C4000%2C3000&w=640&h=480"}},"id":"_Default","slice_type":"image"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":640,"height":590},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1596195689404-24d8a8d1c6ea?rect=0%2C2%2C5000%2C4609&w=640&h=590"}},"id":"_Default","slice_type":"image"}} />
 _Default.storyName = ''
